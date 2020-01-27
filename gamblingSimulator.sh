50372e6bb0294b8be811fb99b5ac5a49b78de465
AvatarMalekar <avatarmalekar4@gmail.com>
2020-01-27 14:12:47 +0530

    ADDED:Luckiest Day and Unluckiest day in gambling

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -7,27 +7,43 @@ TOTAL_DAYS_OF_MONTH=20;
 BET_PER_GAME=1;
 WIN=1;
 LOSE=0;
+maxWinningAmount=-1;
+maxLosingAmmount=-1;
 declare -A GamblerDictionary
-for (( i=0; i<$TOTAL_DAYS_OF_MONTH; i++ ))
+
+for (( i=1; i<=$TOTAL_DAYS_OF_MONTH; i++ ))
 do
 cash_in_hand=$STAKE_FOR_DAY
+countForMaxWin=0
+countForMaxLose=0
 	while [ $cash_in_hand -le $WINNING_LIMIT_TO_RESIGN_FOR_DAY -a $cash_in_hand -ge $LOSING_LIMIT_TO_RESIGN_FOR_DAY ]
 	do
 		bet=$((RANDOM%2))
 		if [ $bet -eq $WIN ]
 		then
+			((countForMaxWin++))
 			cash_in_hand=$(($cash_in_hand+1));
 		else
+			((countForMaxLose++))
 			cash_in_hand=$(($cash_in_hand-1));
 		fi
 	done
-	
 	if [ $cash_in_hand -ge $WINNING_LIMIT_TO_RESIGN_FOR_DAY ]
 	then
 		GamblerDictionary[WON]=$((${GamblerDictionary[WON]}+1))
 	else
 		GamblerDictionary[LOSE]=$((${GamblerDictionary[LOSE]}+1))
 	fi
+	if [ $countForMaxWin -gt $maxWinningAmount ]
+	then
+		maxWinningAmount=$countForMaxWin
+		luckiestDay=$i
+	fi
+	if [ $countForMaxLose -gt $maxLosingAmmount ]
+   then
+      maxLosingAmmount=$countForMaxLose
+      UnLuckiestDay=$i
+   fi
 done
 echo "Total days staticstics:"
 echo ${!GamblerDictionary[@]}
@@ -40,3 +56,8 @@ done
 echo "Total ammount staticstics:"
 echo ${!GamblerDictionary[@]}
 echo ${GamblerDictionary[@]}
+
+echo "Luckiest Day is:" $luckiestDay
+echo "Unluckiest Day is:" $UnLuckiestDay
+echo "Max ammount Won:" $maxWinningAmount
+echo "max ammount Lost:" $maxLosingAmmount
