2138b693c774020d489c93d587d243b5c69af1b5
AvatarMalekar <avatarmalekar4@gmail.com>
2020-01-27 12:21:29 +0530

    ADDED:staticstics of winning ammount and losing ammount for 20 days

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -3,25 +3,32 @@ echo "-----------------------------------------------WELLOME TO GAMBLING SIMULAT
 STAKE_FOR_DAY=100;
 WINNING_LIMIT_TO_RESIGN_FOR_DAY=150;
 LOSING_LIMIT_TO_RESIGN_FOR_DAY=50;
+TOTAL_DAYS_OF_MONTH=20;
 BET_PER_GAME=1;
 WIN=1;
 LOSE=0;
-
-cash_in_hand=$STAKE_FOR_DAY
-while [ $cash_in_hand -le $WINNING_LIMIT_TO_RESIGN_FOR_DAY -a $cash_in_hand -ge $LOSING_LIMIT_TO_RESIGN_FOR_DAY ]
+declare -A GamblerDictionary
+for (( i=0; i<$TOTAL_DAYS_OF_MONTH; i++ ))
 do
-bet=$((RANDOM%2))
-	if [ $bet -eq $WIN ]
+cash_in_hand=$STAKE_FOR_DAY
+	while [ $cash_in_hand -le $WINNING_LIMIT_TO_RESIGN_FOR_DAY -a $cash_in_hand -ge $LOSING_LIMIT_TO_RESIGN_FOR_DAY ]
+	do
+		bet=$((RANDOM%2))
+		if [ $bet -eq $WIN ]
+		then
+			cash_in_hand=$(($cash_in_hand+1));
+		else
+			cash_in_hand=$(($cash_in_hand-1));
+		fi
+	done
+	
+	if [ $cash_in_hand -ge $WINNING_LIMIT_TO_RESIGN_FOR_DAY ]
 	then
-		cash_in_hand=$(($cash_in_hand+1));
+		GamblerDictionary[WON]=$((${GamblerDictionary[WON]}+1*50))
 	else
-		cash_in_hand=$(($cash_in_hand-1));
+		GamblerDictionary[LOSE]=$((${GamblerDictionary[LOSE]}+1*50))
 	fi
 done
-	
-if [ $cash_in_hand -ge $WINNING_LIMIT_TO_RESIGN_FOR_DAY ]
-then
-	echo "Gambler won and resigned...!!"
-else
-	echo "Gambler lost and resigned...!!"
-fi
+echo "Total amount staticstics"
+echo ${!GamblerDictionary[@]}
+echo ${GamblerDictionary[@]}
