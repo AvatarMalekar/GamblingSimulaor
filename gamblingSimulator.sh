214febbcf228b590e974df05c9440824ee72dfcf
AvatarMalekar <avatarmalekar4@gmail.com>
2020-01-27 11:26:13 +0530

    ADDED:gamblers resign condition for winnnig and lossing

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -1,13 +1,27 @@
-#!/bin/bash -x
+#!/bin/bash
 echo "-----------------------------------------------WELLOME TO GAMBLING SIMULATOR-------------------------------------------------"
 STAKE_FOR_DAY=100;
+WINNING_LIMIT_TO_RESIGN_FOR_DAY=150;
+LOSING_LIMIT_TO_RESIGN_FOR_DAY=50;
 BET_PER_GAME=1;
 WIN=1;
 LOSE=0;
+
+cash_in_hand=$STAKE_FOR_DAY
+while [ $cash_in_hand -le $WINNING_LIMIT_TO_RESIGN_FOR_DAY -a $cash_in_hand -ge $LOSING_LIMIT_TO_RESIGN_FOR_DAY ]
+do
 bet=$((RANDOM%2))
-if [ $bet -eq $WIN ]
+	if [ $bet -eq $WIN ]
+	then
+		cash_in_hand=$(($cash_in_hand+1));
+	else
+		cash_in_hand=$(($cash_in_hand-1));
+	fi
+done
+	
+if [ $cash_in_hand -ge $WINNING_LIMIT_TO_RESIGN_FOR_DAY ]
 then
-	echo "CONGRANTS..!! YOU WON.."
+	echo "Gambler won and resigned...!!"
 else
-	echo "SORRY..!! YOU LOSE..!! BETTER LUCK NEXT TIME..!!"
+	echo "Gambler lost and resigned...!!"
 fi
