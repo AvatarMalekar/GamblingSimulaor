09780c99e6c65efad628129131ca2794af967d32
AvatarMalekar <avatarmalekar4@gmail.com>
2020-01-27 10:37:14 +0530

    ADDED:win or lose result for gambler for a bet

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -2,3 +2,12 @@
 echo "-----------------------------------------------WELLOME TO GAMBLING SIMULATOR-------------------------------------------------"
 STAKE_FOR_DAY=100;
 BET_PER_GAME=1;
+WIN=1;
+LOSE=0;
+bet=$((RANDOM%2))
+if [ $bet -eq $WIN ]
+then
+	echo "CONGRANTS..!! YOU WON.."
+else
+	echo "SORRY..!! YOU LOSE..!! BETTER LUCK NEXT TIME..!!"
+fi
