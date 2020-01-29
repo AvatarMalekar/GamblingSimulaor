e34e8da8d22f89de8ec48e0a4184ab332798e958
AvatarMalekar <avatarmalekar4@gmail.com>
2020-01-29 13:14:13 +0530

    REFACTORED:program using functions

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "-----------------------------------------------WELLOME TO GAMBLING SIMULATOR-------------------------------------------------"
+
 #CONSTANT
 STAKE_FOR_DAY=100;
 WINNING_LIMIT_TO_RESIGN_FOR_DAY=150;
@@ -11,61 +12,100 @@ LOSE=0;
 
 #VARIABLE
 flag=0
-while [ $flag -eq 0 ]
-do
 maxWinningAmount=-1;
 maxLosingAmmount=-1;
 declare -A GamblerDictionary
+
+#FUNCTIONS
+function dailyBet(){
+	while [ $cashInHand -le $WINNING_LIMIT_TO_RESIGN_FOR_DAY -a $cashInHand -ge $LOSING_LIMIT_TO_RESIGN_FOR_DAY ]
+	do
+	bet=$((RANDOM%2))
+		if [ $bet -eq $WIN ]
+		then
+			((countForMaxWin++))
+			cashInHand=$(($cashInHand+1));
+		else
+			((countForMaxLose++))
+			cashInHand=$(($cashInHand-1));
+		fi
+	done
+}
+function winOrLoseDecider(){
+	if [ $cashInHand -ge $WINNING_LIMIT_TO_RESIGN_FOR_DAY ]
+	then
+		GamblerDictionary[WON]=$((${GamblerDictionary[WON]}+1))
+	else
+		GamblerDictionary[LOSE]=$((${GamblerDictionary[LOSE]}+1))
+	fi
+}
+function luckiestDayDecider(){
+	if [ $countForMaxWin -gt $maxWinningAmount -a $cashInHand -ge $WINNING_LIMIT_TO_RESIGN_FOR_DAY ]
+	then
+		maxWinningAmount=$countForMaxWin
+		luckiestDay=$i
+	fi
+}
+function unLuckiestDayDecider(){
+	if [ $countForMaxLose -gt $maxLosingAmmount -a $cashInHand -le $LOSING_LIMIT_TO_RESIGN_FOR_DAY ]
+   then
+      maxLosingAmmount=$countForMaxLose
+      UnLuckiestDay=$i
+   fi
+}
+function monthlyWinOrLose(){
+	for i in ${!GamblerDictionary[@]}
+	do
+		GamblerDictionary[$i]=$((${GamblerDictionary[$i]}*50))
+	done
+}
+function showKeyValueOfDictionary(){
+	echo ${!GamblerDictionary[@]}
+	echo ${GamblerDictionary[@]}
+}
+
+function clearDictionary(){
+	for i in ${!GamblerDictionary[@]}
+	do
+   	GamblerDictionary[$i]=0
+	done
+}
+
+#MAIN-FUNCTION
+while [ $flag -eq 0 ]
+do
 	for (( i=1; i<=$TOTAL_DAYS_OF_MONTH; i++ ))
 	do
-		cash_in_hand=$STAKE_FOR_DAY
+		cashInHand=$STAKE_FOR_DAY
 		countForMaxWin=0
 		countForMaxLose=0
-		while [ $cash_in_hand -le $WINNING_LIMIT_TO_RESIGN_FOR_DAY -a $cash_in_hand -ge $LOSING_LIMIT_TO_RESIGN_FOR_DAY ]
-		do
-			bet=$((RANDOM%2))
-			if [ $bet -eq $WIN ]
-			then
-				((countForMaxWin++))
-				cash_in_hand=$(($cash_in_hand+1));
-			else
-				((countForMaxLose++))
-				cash_in_hand=$(($cash_in_hand-1));
-			fi
-		done
-		if [ $cash_in_hand -ge $WINNING_LIMIT_TO_RESIGN_FOR_DAY ]
-		then
-			GamblerDictionary[WON]=$((${GamblerDictionary[WON]}+1))
-		else
-			GamblerDictionary[LOSE]=$((${GamblerDictionary[LOSE]}+1))
-		fi
-		if [ $countForMaxWin -gt $maxWinningAmount -a $cash_in_hand -ge $WINNING_LIMIT_TO_RESIGN_FOR_DAY ]
-		then
-			maxWinningAmount=$countForMaxWin
-			luckiestDay=$i
-		fi
-		if [ $countForMaxLose -gt $maxLosingAmmount -a $cash_in_hand -le $LOSING_LIMIT_TO_RESIGN_FOR_DAY ]
-   	then
-      	maxLosingAmmount=$countForMaxLose
-      	UnLuckiestDay=$i
-   	fi
-done
-echo "Total days staticstics:"
-echo ${!GamblerDictionary[@]}
-echo ${GamblerDictionary[@]}
 
-for i in ${!GamblerDictionary[@]}
-do
-	GamblerDictionary[$i]=$((${GamblerDictionary[$i]}*50))
-done
+		#FUNCTION-CALLS
+		dailyBet
+		winOrLoseDecider
+		luckiestDayDecider
+		unLuckiestDayDecider
+	done
+
+echo "Total days staticstics:"
+
+#FUNCTION-CALLS
+showKeyValueOfDictionary
+monthlyWinOrLose
+
 echo "Total ammount staticstics for a month:"
-echo ${!GamblerDictionary[@]}
-echo ${GamblerDictionary[@]}
+
+#FUNCTION-CALL
+showKeyValueOfDictionary
 
 echo "Luckiest Day is:" $luckiestDay
 echo "Unluckiest Day is:" $UnLuckiestDay
 echo "Max ammount Won:" $maxWinningAmount
 echo "max ammount Lost:" $maxLosingAmmount 
+
+#FUNCTION-CALL
+clearDictionary
+
 read -p "Do you want to continue for next month..(y/n): " result
 if [ "$result" = "Y" -o "$result" = "y" ]
 then
@@ -73,10 +113,5 @@ then
 else
 	flag=1;
 fi
-
-for i in ${!GamblerDictionary[@]}
-do
-   GamblerDictionary[$i]=0
-done
 
 done
